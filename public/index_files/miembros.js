62ff6c51d0ee8eec8e874832f48227dc44744652
DESKTOP-5NNAD6U\Antonio Miguel <antonnio762@gmail.com>
2025-03-20 22:41:04 -0500

    miembros

--- public/index_files/miembros.js
+++ public/index_files/miembros.js
@@ -1,24 +1,22 @@
 fetch('miembros.json')
   .then(response => response.json())
   .then(data => {
-    const contenedor = document.getElementById('contenedorNombres');
+    const contenedor = document.getElementById('contenedorMiembros');
 
     data.miembros.forEach(miembro => {
-      // Selecciona el div contenedor existente
       const div = document.createElement('div');
-      
+      div.className = 'item';
+
       div.innerHTML = `
-        <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-1">
-          <h3 class="u-text u-text-default u-text-2 animated customAnimationIn-played" 
-              data-animation-direction="Down" 
-              data-animation-name="customAnimationIn" 
-              data-animation-duration="1500" 
-              data-animation-delay="1250" 
-              style="will-change: transform, opacity; animation-duration: 1500ms;">
-            &nbsp;${miembro.nombreCompleto}
-          </h3>
-        </div>`;
-      
+        <img  src="https://firebasestorage.googleapis.com/v0/b/alfa-orion-2291d.firebasestorage.app/o/usuario-1.jpg?alt=media" 
+          data-image-width="1335" data-image-height="2000"
+          data-animation-name="flipIn" data-animation-duration="3000" data-animation-delay="0"
+          data-animation-direction="X" style="will-change: transform; animation-duration: 500ms; height: 325px;
+  width: 275px;">
+        <h5>${miembro.nombreCompleto}</h5>
+        <h6>${miembro.cargo}</h6>
+      `;
+
       contenedor.appendChild(div);
     });
   })
